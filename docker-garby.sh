b74f48789efd9f3551678f1429acd018e098e23c
Thomas Sjögren <konstruktoid@users.noreply.github.com>
2016-01-04 23:20:40 +0100

    dont repeat docker rm
    Signed-off-by: Thomas Sjögren <konstruktoid@users.noreply.github.com>

--- docker-garby.sh
+++ docker-garby.sh
@@ -22,37 +22,30 @@ containerRemoval(){
     imageName=$(docker inspect -f '{{.RepoTags}}' "$containerImage")
     timeDiffOutput=$(timeDiff "$containerFinished")
     echo "$containerImage" >> "$usedImagesLog"
+    remove=0
 
     if [ "$containerStatus" = "created" -a "$containerRunningState" = "false" ]; then
       logAllThings "Container $containerName ($con) is in 'created' state."
-      docker rm "$con" 2>/dev/null 1>&2
-
-      if [ "$?" -eq 0 ]; then
-        logAllThings "Container $containerName ($con) removed."
-        else
-        logAllThings "ERR: Container $containerName ($con) was not removed."
-      fi
+      remove=1
     fi
 
     if [ "$containerDead" = "true" -a "$containerRunningState" = "false" ]; then
       logAllThings "Container $containerName ($con) is in 'dead' state."
-      docker rm "$con" 2>/dev/null 1>&2
-
-      if [ "$?" -eq 0 ]; then
-        logAllThings "Container $containerName ($con) removed."
-        else
-        logAllThings "ERR: Container $containerName ($con) was not removed."
-      fi
+      remove=1
     fi
 
     if [ "$timeDiffOutput" -gt $maxSecondsOld -a "$containerRunningState" = "false" ]; then
       logAllThings "Container $containerName ($con) finished $timeDiffOutput seconds ago."
-      logAllThings "Container $containerName ($con) used image $imageName."
-      docker rm "$con" 2>/dev/null 1>&2
+      remove=1
+    fi
 
+    if [ "$remove" = 1 ]; then
+      logAllThings "Container $containerName ($con) used image $imageName."
+
+      docker rm "$con" 2>/dev/null 1>&2
       if [ "$?" -eq 0 ]; then
         logAllThings "Container $containerName ($con) removed."
-        else
+      else
         logAllThings "ERR: Container $containerName ($con) was not removed."
       fi
     fi
